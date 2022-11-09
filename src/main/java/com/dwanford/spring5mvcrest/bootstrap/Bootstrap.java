65d49d94a684a77832eb8e5444fe978d83389194
dwanford <dwanford@hotmail.com>
2022-11-09 14:06:48 +0200

    bootstrap vendor data

--- src/main/java/com/dwanford/spring5mvcrest/bootstrap/Bootstrap.java
+++ src/main/java/com/dwanford/spring5mvcrest/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package com.dwanford.spring5mvcrest.bootstrap;
 
 import com.dwanford.spring5mvcrest.domain.Category;
 import com.dwanford.spring5mvcrest.domain.Customer;
+import com.dwanford.spring5mvcrest.domain.Vendor;
 import com.dwanford.spring5mvcrest.repositories.CategoryRepository;
 import com.dwanford.spring5mvcrest.repositories.CustomerRepository;
+import com.dwanford.spring5mvcrest.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +16,12 @@ public class Bootstrap implements CommandLineRunner {
 
     private final CategoryRepository categoryRepository;
     private final CustomerRepository customerRepository;
+    private final VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
@@ -25,9 +29,7 @@ public class Bootstrap implements CommandLineRunner {
 
         loadCategories();
         loadCustomers();
-
-        System.out.println("Category data loaded  = " + categoryRepository.count());
-        System.out.println("Customer data loaded  = " + customerRepository.count());
+        loadVendors();
     }
 
     private void loadCategories() {
@@ -48,6 +50,7 @@ public class Bootstrap implements CommandLineRunner {
 
 
         categoryRepository.saveAll(Arrays.asList(fruits, dried, fresh, exotic, nuts));
+        System.out.println("Category data loaded  = " + categoryRepository.count());
     }
 
     private void loadCustomers() {
@@ -64,5 +67,17 @@ public class Bootstrap implements CommandLineRunner {
         travisBickle.setLastName("Bickle");
 
         customerRepository.saveAll(Arrays.asList(johnDoe, patrickBateman, travisBickle));
+        System.out.println("Customer data loaded  = " + customerRepository.count());
+    }
+
+    private void loadVendors() {
+        Vendor vendor1 = new Vendor();
+        vendor1.setName("Pierce & Pierce");
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setName("Lou Bloom");
+
+        vendorRepository.saveAll(Arrays.asList(vendor1, vendor2));
+        System.out.println("Vendor data loaded  = " + vendorRepository.count());
     }
 }
