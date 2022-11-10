9211b7a00e64572e9cc64ead2e9c3190e17d057f
dwanford <dwanford@hotmail.com>
2022-11-10 14:29:33 +0200

    added OpenAPI 3, Swagger

--- src/main/java/com/dwanford/spring5mvcrest/controllers/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/dwanford/spring5mvcrest/controllers/RestResponseEntityExceptionHandler.java
@@ -6,12 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler {
 
     @ExceptionHandler({ResourceNotFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ResponseEntity<Object> handleNotFoundException(Exception exception, WebRequest request) {
         return new ResponseEntity<>("Resource Not Found", new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
